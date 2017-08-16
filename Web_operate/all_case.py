01d57187c678febe6a2f1cbd8274e01ce4acfac0
Nuoxxx <yesiwillbetherexxx@gmail.com>
2017-08-16 17:26:16 +0800

    借助allcase_list添加用例

--- Web_operate/all_case.py
+++ Web_operate/all_case.py
@@ -4,20 +4,17 @@ import sys,os
 import unittest
 import HTMLTestRunner
 
-sys.path.append("\selenum")
-
+sys.path.append("\Selenium_Learn")
+from  Selenium_Learn.Web_operate import allcase_list
 testsuite = unittest.TestSuite()
+#借助allcase_list添加用例
+alltestnames = allcase_list.caselist()
 
-testsuite.addTest(unittest.makeSuite(start_baidu.Baidu))
-testsuite.addTest(unittest.makeSuite(youdao.Youdao))
-
-# alltestnames = allcase_list.caselist()
-#
-# for test in alltestnames:
-#     testsuite.addTest(unittest.makeSuite(test))
+for test in alltestnames:
+    testsuite.addTest(unittest.makeSuite(test))
 
 
-filename = os.path.abspath(os.path.join(os.path.abspath(__file__),"../testcase/report/result.html"))
+filename = os.path.abspath(os.path.join(os.path.abspath(__file__),"../report/result.html"))
 fp = open(filename,'wb')
 
 runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'测试报告',description='用例执行情况')
--- Web_operate/allcase_list.py
+++ Web_operate/allcase_list.py
@@ -2,12 +2,13 @@ import sys
 import unittest
 import HTMLTestRunner
 
-sys.path.append("\ceses")
-from ceses import youdao
-from ceses import start_baidu
+sys.path.append("\Selenium_Learn")
 
+from Selenium_Learn.Web_operate.ceses import start_baidu
+from Selenium_Learn.Web_operate.ceses import youdao
 
 def caselist():
+    #用该list专门用于添加测试用例
     alltestnames = [start_baidu.Baidu, youdao.Youdao]
     print(sys.path)
     print("Success read case list!!!")
