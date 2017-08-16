9c3e5aa90156bb414932cafb51f5145bdbfb9f77
Nuoxxx <yesiwillbetherexxx@gmail.com>
2017-08-16 17:27:11 +0800

    通过unittest.makeSuite添加测试用例

---
+++ Web_operate/all_case_makeSuite.py
@@ -0,0 +1,21 @@
+# coding = utf-8
+
+import sys,os
+import unittest
+import HTMLTestRunner
+
+sys.path.append("\Selenium_Learn")
+from Selenium_Learn.Web_operate.ceses import start_baidu
+from Selenium_Learn.Web_operate.ceses import youdao
+
+testsuite = unittest.TestSuite()
+# 通过unittest.makeSuite添加待执行用例
+testsuite.addTest(unittest.makeSuite(start_baidu.Baidu))
+testsuite.addTest(unittest.makeSuite(youdao.Youdao))
+
+
+filename = os.path.abspath(os.path.join(os.path.abspath(__file__),"../report/result.html"))
+fp = open(filename,'wb')
+
+runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'测试报告',description='用例执行情况')
+runner.run(testsuite)
